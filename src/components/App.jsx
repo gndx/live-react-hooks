818c13c626bd3819dd13fcb45658d8c8e15eb34c
Oscar Barajas <oscar@arepa.dev>
2020-08-13 16:44:44 -0500

    Actions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import '../styles/components/App.styl';
+import useCharacters from '../hooks/useCharacters';
 
-const App = () => <h1>Hello React!</h1>;
+const App = () => {
+  const characters = useCharacters(
+    'https://rickandmortyapi.com/api/character/'
+  );
+  return (
+    <div className="App">
+      {characters.map(character => (
+        <div className="App-content">
+          <h2>{character.name}</h2>
+          <img src={character.image} alt={character.name} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
